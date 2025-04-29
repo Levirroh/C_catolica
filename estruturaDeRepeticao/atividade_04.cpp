ca098bbbdaba3458e57b15e7ceeecea6f6605801
Johann Gossen Ruth <146986028+Levirroh@users.noreply.github.com>
2025-04-28 21:51:17 -0300

    Create atividade_04.cpp

---
+++ estruturaDeRepeticao/atividade_04.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	
+	float media, nota;
+	int quantidade;
+	
+	printf("Digite o total de notas: \n");
+	scanf("%i", quantidade);
+	fflush(stdin);
+	
+	for (int n = 1; n <= 10; n++){
+			
+	
+	}
+	printf("A diferenca entre os dois eh %i", maior-menor);
+	
+	
+	return 0;
+}
